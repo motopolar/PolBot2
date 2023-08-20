502a017cfa3e9cf9c837a363b0d128596637dc9b
motopolar <120253012+motopolar@users.noreply.github.com>
2023-08-19 23:03:36 -0700

    PolBot

--- plugins/gc-admins.js
+++ plugins/gc-admins.js
@@ -5,11 +5,11 @@ const listAdmin = groupAdmins.map((v, i) => `${i + 1}. @${v.id.split('@')[0]}`).
 const owner = groupMetadata.owner || groupAdmins.find(p => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net'
 let pesan = args.join` `
 let oi = `*𝙼𝙴𝙽𝚂𝙰𝙹𝙴:* ${pesan}`
-let text = `⟥⟝⟢⟨ *𝐈𝐍𝐕𝐎C-𝐀𝐃𝐌𝐈𝐍𝐒* ⟩⟣⟞⟤ 
+let text = `⟥⟝⟢⟨ *Invocando Administradores* ⟩⟣⟞⟤ 
 •${oi}
 • *𝙰𝙳𝙼𝙸𝙽𝚂:*
 • ${listAdmin}
-⬒─⟢⟨©𝙷𝙰𝚃𝚂𝚄𝙽𝙴-𝙼𝙸𝙺𝚄-𝚄𝙻𝚃𝚁𝙰✍⟩⟣─⬒`.trim()
+⬒─⟢⟨PolBot⟩⟣─⬒`.trim()
 conn.sendFile(m.chat, pp, 'error.jpg', text, m, false, { mentions: [...groupAdmins.map(v => v.id), owner] })
 }
 handler.help = ['admins <texto>']
